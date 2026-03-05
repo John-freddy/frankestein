8728d62053f2a69c1d2b443641ea6a41d3e0999b
John-freddy <jfvalencia@hotmail.com>
2026-03-04 19:12:52 -0500

    chore: update 20260304-1912

--- backup.bat
+++ backup.bat
@@ -9,7 +9,7 @@ git status -sb
 findstr /x /c:"backups/" .gitignore >nul 2>nul || echo backups/>>.gitignore
 findstr /x /c:"node_modules_old/" .gitignore >nul 2>nul || echo node_modules_old/>>.gitignore
 
-git add -A -- . ":(exclude)node_modules_old" ":(exclude)backups"
+git -c advice.addIgnoredFile=false add -A -- . ":(exclude)node_modules_old" ":(exclude)backups"
 
 for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd-HHmm"') do set TS=%%i
 
